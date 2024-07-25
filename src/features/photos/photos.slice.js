d33d1a47482246a268856d6e6f2e7966ab91a665
lois <sloi@indeed.com>
2024-07-25 17:36:25 +0900

    added gh-pages

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -10,13 +10,20 @@ const options = {
   name: 'photos',
   initialState,
   reducers: {
-    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
-    // Task 1 Hint: You can use state.photos.unshift()
-    // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
-   
+    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos.
+    addPhoto: (state, action) => {
+        state.photos.unshift({ // unshift for adding to the top
+            id: state.photos.length + 1,
+            imageUrl: action.payload.imageUrl,
+            caption: action.payload.caption,
+        });
+    },
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
-    // Task 6 Hint: You can use state.photos.splice()
-    // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+   removePhoto: (state, action) => {
+    state.photos.splice(
+        state.photos.findIndex((photo) => photo.id === action.payload), 1
+    )
+   }
   },
 };
 
@@ -27,6 +34,12 @@ export const { addPhoto, removePhoto } = photosSlice.actions;
 export default photosSlice.reducer;
 
 export const selectAllPhotos = (state) => state.photos.photos;
-export const selectFilteredPhotos = (state) => {
-  // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
+
+export const selectFilteredPhotos = (state) => { // need it to search the caption
+    // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
+    const photos = selectAllPhotos(state);
+    const searchTerm = selectSearchTerm(state);
+
+    return photos.filter((item) => // use the caption
+        item.caption.toLowerCase().includes(searchTerm.toLowerCase()))
 };
